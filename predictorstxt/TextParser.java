6274b85bf7b5c0aedd4f992134b7baf8fdf86780
nadyac <nadyacpena@gmail.com>
2014-05-08 18:19:22 -0400

    Added code to output hasCategory file

--- predictorstxt/TextParser.java
+++ predictorstxt/TextParser.java
@@ -14,9 +14,13 @@ public class TextParser{
 
 	public static void main(String[] argsv) throws IOException{
 
-		//-------------------------------------------------------------------
-		// Print out how many missing values there are for alchemy_category
-		//-------------------------------------------------------------------
+		//--------------------------------------------------------------------------
+		// Declare global variables (so they may be used outside try catch clauses)
+		//--------------------------------------------------------------------------
+		int missingvalues = 0;
+		int i =0;
+		int totalEvergreen = 0;
+		
 		String[] filenames = {"alchemy_category.txt", "alchemy_category_score.txt", "avglinksize.txt",
 								"commonlinkratio_1.txt", "commonlinkratio_2.txt", "commonlinkratio_3.txt",
 								"commonlinkratio_4.txt", "compression_ratio.txt", "embed_ratio.txt", "framebased.txt",
@@ -24,11 +28,27 @@ public class TextParser{
 								"is_news.txt", "label.txt", "lengthyLinkDomain.txt", "linkwordscore.txt", "news_front_page.txt",
 								"non_markup_alphanum_characters.txt", "numberOfLinks.txt", "numwords_in_url.txt",
 								"parameterizedLinkRatio.txt", "spelling_errors_ratio.txt"};
+	//------------------------------------------------------------------------
+	// Create the output file where the hasCategory values will be written
+	//------------------------------------------------------------------------
+	try{
 
-		for(int i = 0; i < filenames.length; i++){
+			File categoryFile = new File("hasCategory.txt");
+
+			if(!categoryFile.exists()){
+				categoryFile.createNewFile();
+			}
+
+			FileWriter categoryWriter = new FileWriter(categoryFile.getAbsoluteFile());
+			BufferedWriter catWriter = new BufferedWriter(categoryWriter);
+		
+		//-------------------------------------------------------------------
+		// Print out how many missing values there are for each variable
+		//-------------------------------------------------------------------
+		for(i = 0; i < filenames.length; i++){
 
 			File txtFile = new File(filenames[i]);
-			int missingvalues = 0;
+			missingvalues = 0;
 
 			if(!txtFile.exists()){
 				System.out.println("The file does not exist!");
@@ -42,8 +62,30 @@ public class TextParser{
 				if(predictor_value.equals("?")){
 					missingvalues++;
 				} 
-			}
+				//------------------------------------------------------------------------------
+				// Populate the hasCategory.txt file. 
+				//------------------------------------------------------------------------------
+				if(filenames[i].equals("alchemy_category.txt") && predictor_value.equals("?")){
+					catWriter.write(""+"0");
+					catWriter.newLine();
+				}else{
+					catWriter.write(""+"1");
+					catWriter.newLine();
+				}
+				//-----------------------------------------------------------------------------
+				// Check how many Evergreen links there are
+				//-----------------------------------------------------------------------------
+				if(filenames[i].equals("label.txt") && predictor_value.equals("1")){
+					totalEvergreen++;
+				}
+			} 
 			System.out.println("There are " + missingvalues + " missing in variable " + filenames[i] + " \n");
 		}
+		catWriter.close();
+	} catch (IOException ex){
+			System.out.println("Problem creating the file");
+	   }
+	   
+	System.out.println("The number of Evergreen links in the training set are: " + totalEvergreen);
 	}
 }
